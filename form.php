fe7d6e20e3a75c09be8e1d962dc4f0d4814e3c24
sabrina <sabourodrigues@hotmail.fr>
2021-03-28 11:19:17 +0200

    result challenge securisation form

--- form.php
+++ form.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,26 +7,31 @@
     <title>Document</title>
 </head>
 <body>
-<form  action="thanks.php"  method="post">
+
+<form  action="thanks.php" method="post">
     <div>
       <label  for="lastName">Nom :<br></label>
-      <input  type="text"  id="lastName"  name="user_name">
+      <input  type="text"  id="lastName"  name="user_name" required >
+      
     </div>
     <div>
       <label  for="firstName">Prénom:<br></label>
-      <input  type="text"  id="firstName"  name="user_firstName">   
+      <input  type="text"  id="firstName"  name="user_firstName" required>
+      
     </div>
     <div>
       <label  for="mail">Your mail :<br></label>
-      <input  type="email"  id="mail"  name="user_email">
+      <input  type="email"  id="mail"  name="user_email" required >
+      
     </div>
     <div>
       <label for="phone">Enter your phone number:<br></label>
-      <input type="tel" id="phone" name="phone">
+      <input type="tel" id="phone" name="phone" required>
+      
     </div>
     <div>
       <label for="subject">choose the subject of your message <br></label>
-      <select name="sujet" id="subject">
+      <select name= "sujet" id="subject" required >
         <option value="">Please choose an option </option>
         <option value="information">Information</option>
         <option value="reclamation">Reclamation</option>
@@ -35,10 +40,11 @@
     </div>
     <div>
       <label  for="message">Message :<br></label>
-      <textarea  id="message"  name="user_message"></textarea>
+      <textarea  id="message"  name="user_message" required ></textarea>
+      
     </div>
     <div  class="button">
-      <button  type="submit">Envoyer votre message <br></button>
+      <button  type="submit">Envoyer votre message<br></button>
     </div>
   </form>  
 </body>
--- thanks.php
+++ thanks.php
@@ -1,6 +1,39 @@
 <?php
-  echo 'Merci '. $_POST['user_firstName'] .' ' . $_POST["user_name"].' de nous avoir contacté à propose de :'.$_POST["sujet"] . '.' .'<br>' .
-'Un de nos conseiller vous contactera soit à l\'adresse ' . $_POST["user_email"] . ' ou par téléphone au : '. $_POST["phone"] . ' dans les plus brefs délais pour traiter votre demande :'.'<br>'
- .$_POST["user_message"];
+$data = array_map('trim', $_POST);
+$errors = [];
+   
+if(empty ($data["user_name"])){
+        $errors[] = "Name is required";
+    }
+if(empty ($data["user_firstName"])){
+        $errors[] = "firstname is required";
+    }
+if(empty ($data["user_email"])){
+        $errors[] = "Email is required";
+    }
+if(empty ($data["phone"])){
+        $errors[] = "phone is required";
+    }
+ if(empty ($data["sujet"])){
+        $errors[] = "Subject of your message is required";
+    }
+    if(empty ($data["user_message"])){
+        $errors[] = "Your message is required";
+    }
+    if (!filter_var($data["user_email"], FILTER_VALIDATE_EMAIL)){
+        $errors[] = "Invalid email format";
+    }
+    if (!empty($errors)) {
+        for($i=0;$i< count($errors);$i++) {
+          echo $errors[$i]. "<br>";
+        }
+    } 
+else{
+
+    echo 'Merci '. $_POST['user_firstName'] .' ' . $_POST["user_name"].' de nous avoir contacté à propose de :'.$_POST["sujet"] . '.' .'<br>' .
+        'Un de nos conseiller vous contactera soit à l\'adresse ' . $_POST["user_email"] . ' ou par téléphone au : '. $_POST["phone"] . ' dans les plus brefs délais pour traiter votre demande :'.'<br>'
+        .$_POST["user_message"];
+}
+
 
 ?>
